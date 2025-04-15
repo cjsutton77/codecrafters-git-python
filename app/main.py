e8bdbde0885449204a0fb5336e1225b22ae15388
Christian Sutton <christian.sutton@gmail.com>
2025-04-14 21:59:08 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import zlib
 
 
 def main():
@@ -16,6 +17,16 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+    elif (sys.argv[1] == "cat-file" and sys.argv[2] == "-p"):
+        blobHash = sys.argv[3]
+        with open(f".git/objects/{blobHash[:2]}/{blobHash[2:]}", "rb") as f:
+            # Read and decompress the data from the file using zlib
+            raw = zlib.decompress(f.read())
+            # Split the decompressed data into a header and content at the first null byte
+            header, content = raw.split(b"\0", maxsplit=1)
+            # Print the content part, decoding it from bytes to a UTF-8 string
+            print(content.decode(), end="")
+
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
