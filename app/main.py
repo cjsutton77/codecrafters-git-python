08504d2c47ad5b583196879e22c79c788d8e2c48
Christian Sutton <christian.sutton@gmail.com>
2025-04-16 21:02:51 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import zlib
+import hashlib
 
 
 def main():
@@ -26,10 +27,20 @@ def main():
             header, content = raw.split(b"\0", maxsplit=1)
             # Print the content part, decoding it from bytes to a UTF-8 string
             print(content.decode(), end="")
-
+    elif (sys.argv[1] == "hash-object" and sys.argv[2] == "-w"):
+        with open(f"{sys.argv[3]}", "rb") as f:
+            obj = f.read()
+            # Add header
+            result = b'blob ' + str(len(obj)).encode() + b'\x00' + obj
+            # # Compute hash
+            sha = hashlib.sha1(result).hexdigest()
+            # Write hash
+            print(sha)
+            os.mkdir(f".git/objects/{sha[:2]}")
+            with open(f".git/objects/{sha[:2]}/{sha[2:]}", "wb") as fd:
+                fd.write(zlib.compress(result))
     else:
         raise RuntimeError(f"Unknown command #{command}")
-
 
 if __name__ == "__main__":
     main()
